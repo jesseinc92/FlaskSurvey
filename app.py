1479fd16d69b1f5e56f0eee8279aaec197874123
Jesse Hill <jesse.o.hill@gmail.com>
2021-08-30 23:07:48 -0400

    added home and question templates

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+from surveys import satisfaction_survey
+
+app = Flask(__name__)
+
+responses = []
+
+@app.route('/')
+def survey_start():
+    title = satisfaction_survey.title
+    instructions = satisfaction_survey.instructions
+    return render_template('home.html', survey_title=title, survey_instructions=instructions)
+
+@app.route('question/<int:id>')
+def survey_question(id):
+    question = satisfaction_survey.questions[id]
+    return render_template('question.html', survey_question=question)
